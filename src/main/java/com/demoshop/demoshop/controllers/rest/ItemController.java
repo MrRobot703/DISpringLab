a29fe4452b0714576fe93becbddf8ae7c8d4dc50
ShuryginaA <annashurygina353@gmail.com>
2021-06-04 10:54:02 +0300

    Delete backend folder from /test, fixed imports.

---
+++ src/main/java/com/demoshop/demoshop/controllers/rest/ItemController.java
@@ -0,0 +1,53 @@
+package com.demoshop.demoshop.controllers.rest;
+
+import com.demoshop.demoshop.data.dto.simple.ItemDto;
+import com.demoshop.demoshop.data.entity.ItemEntity;
+import com.demoshop.demoshop.service.api.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+//TODO:exceptions
+
+@RestController
+public class ItemController {
+
+    @Autowired
+    @Qualifier("actualEntityService")
+    ItemService entService;
+
+    @GetMapping("/test")
+    public String sayHello() {
+        return "Hello, world!";
+    }
+
+    @PostMapping("/saveItem")
+    public String sayItemByPost(@RequestBody ItemDto item) {
+        Object obj = entService.save(item);
+        return obj.toString();
+    }
+
+    @GetMapping("/getAllItems")
+    public List<ItemDto> getAllItems() {
+        return entService.getAllItems();
+    }
+
+    @PostMapping("/getItemById")
+    public ItemEntity getItemById(@RequestBody Long id) {
+        return entService.getItemById(id);
+    }
+
+    @PostMapping("/deleteItem")
+    public void deleteItem(@RequestBody Long id) {
+        entService.delete(id);
+    }
+
+    @PutMapping("/updateItem")
+    public void updateItem(@RequestBody ItemEntity item) {
+        entService.update(item);
+    }
+
+
+}
---
+++ src/main/java/com/demoshop/demoshop/service/api/ItemService.java
@@ -0,0 +1,17 @@
+package com.demoshop.demoshop.service.api;
+
+import com.demoshop.demoshop.data.entity.ItemEntity;
+import com.demoshop.demoshop.data.dto.simple.ItemDto;
+
+import java.util.List;
+
+public interface ItemService {
+    ItemEntity save(ItemDto dto);
+    List<ItemDto> getAllItems();
+    ItemEntity update(ItemEntity item);
+    ItemDto convertToItemDto(ItemEntity itEntity);
+    ItemEntity getItemById(Long id);
+    void delete(Long id);
+
+
+}
