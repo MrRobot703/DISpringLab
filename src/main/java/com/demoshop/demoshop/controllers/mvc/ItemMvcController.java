69f3de64728531dee044896430ba11e9eb18a50f
ShuryginaA <annashurygina353@gmail.com>
2021-06-21 20:19:26 +0300

    Add logging.

--- src/main/java/com/demoshop/demoshop/controllers/mvc/ItemMvcController.java
+++ src/main/java/com/demoshop/demoshop/controllers/mvc/ItemMvcController.java
@@ -1,5 +1,6 @@
 package com.demoshop.demoshop.controllers.mvc;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,15 +8,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
+@Slf4j
 @Controller
 public class ItemMvcController {
 
     @GetMapping({"/item", "/item{id}"})
     public String getItems(@PathVariable(required = false) Long id) {
-        if (id == null)
+        if (id == null) {
+            log.info("Referred to the home page ");
             return "item/itemList";
-        else
+        } else {
+            log.info("Referred to the item page");
             return "item/item" + id;
+        }
     }
 }
 
